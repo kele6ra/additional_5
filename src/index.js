6b7586498865a7b9cda90d7c8f05cb83e44cea29
unknown <Andrei Auchinnikau>
2018-02-22 01:50:24 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  	var a='((()))()';
+    var tmp = a.split('');
+    var b=[['(', ')'], ['[', ']'], ['{', '}']];
+    var i,j;
+    for (i=0;i<(tmp.length);i++){
+      for (j=0; j<b.length;j++){
+      
+		if ((a.indexOf(b[j][0])>=0) && (a.indexOf(b[j][1])>=0)){
+        	
+        	tmp.splice(a.indexOf(b[j][0]),1);
+            tmp.splice(a.indexOf(b[j][1]),1);
+        }
+      }
+    }
 }
